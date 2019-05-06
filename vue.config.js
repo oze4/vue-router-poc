61179657a4bcea9f5d122b0790f0ea8f2f9edd93
Matt Oestreich <matthewpoestreich@gmail.com>
2019-05-05 22:43:38 -0500

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,21 +1,4 @@
-const CopyPlugin = require('copy-webpack-plugin')
-
 module.exports = {  
-  configureWebpack: {
-    //entry: ['babel-polyfill', './src/main.js'],
-    plugins: [
-      new CopyPlugin([{
-        /**
-         * This is needed for GitHub Pages to work correctly it copies
-         * the CNAME file (at the root of project) into the /docs directory after a rebuild.
-         */
-        from: 'CNAME',
-        to: 'CNAME',
-        toType: 'file',
-      }])
-    ],
-  },
-
   publicPath: '',
   outputDir: './docs',
   assetsDir: 'assets'
